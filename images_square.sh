5f9641402e8d77dc5eba0987b7de9a6d0444a023
KJ Coop <git@kjcoop.com>
2019-06-05 07:04:22 -0700

    Renamed image script to specify that the thumbnails it produces are square as opposed to scaled.

---
+++ images_square.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# Take a directory of images and create thumbnails.
+#
+# The thumbnails are squares from the center of images as opposed to 
+# shrinking the image
+#
+# See http://www.imagemagick.org/Usage/thumbnails/ heading "Cut the 
+# Thumbnail to Fit"
+
+# Arguments
+# $1 - path to the images 
+# $2 - new suffix
+# $3 - new size
+
+usage="$0 [path] [suffix] [size]"
+
+if [ "$1" == "" ] || [ ! -d "$1" ]; then
+    echo Must include a path to a directory as the first argument
+    echo $usage
+    exit
+else
+    path=$1
+fi
+
+if [ "$2" == "" ]; then
+    echo You must specify a new suffix for the images in the second argument
+    echo $usage
+    exit
+else
+    suffix=$2
+fi
+
+if [ "$3" == "" ]; then
+    # Too lazy to confirm it's an integer; image magick will know to fail.
+    # But if/when I do want to, SO is here for me:
+    # https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
+    echo You must supply a size for the new images to be in the third argument
+    echo $usage
+    exit
+else 
+    size=$3
+    dim_str="$size"x"$size" # 200x200 or whatever
+fi
+
+for img in $path/*; do
+    # Creating the new image name involves stripping the extension. There's
+    # got to be a more elegant way, but this is what the inimitable Stack
+    # Overflow provided:
+    # https://stackoverflow.com/questions/965053/extract-filename-and-extension-in-bash
+    filename=$(basename -- "$img")
+    extension="${filename##*.}"
+    new_name=$path/"${filename%.*}$suffix"
+
+#    echo Path:      $path
+#    echo File:      $img
+#    echo Filename:  $filename
+#    echo Extension: $extension
+#    echo New name:  $new_name
+
+    # convert -define jpeg:size=200x200 5.jpg -thumbnail 200x200^ -gravity center -extent 200x200 5.th.jpg
+    echo convert -define jpeg:size=$dim_str $img -thumbnail $dim_str^ -gravity center -extent $dim_str $new_name
+    convert -define jpeg:size=$dim_str $img -thumbnail $dim_str^ -gravity center -extent $dim_str $new_name
+done
