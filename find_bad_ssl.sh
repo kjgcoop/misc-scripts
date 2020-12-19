815170d249a565c8ecfd8316304f5760ad3cf774
KJ Coop <git@kjcoop.com>
2020-12-19 00:26:59 -0800

    Now with response times.

--- find_bad_ssl.sh
+++ find_bad_ssl.sh
@@ -3,20 +3,16 @@
 # Find sites that don't have valid global SSL certs. It can go off a list of
 # domains in a file specified below, or domains can be itemized at the command
 # line
-file=domains.txt
+file=~/domain_scripts/domains.txt
 
-# @todo: Suffix domain line with request time. Some domains on the same meachine
-#     seem to take forever. This wouldn't say why, it would just be interesting.
-# @todo: Only list tlds/subdomains
-# @todo: Comment out domains in list
-# @todo: greps out lines from the file with a #. The idea is to be able to
-#     comment out lines, but any URLs with an anchor tag will be a casualty. If
-#     I go to the trouble of checking if it's the first character, then I can
-#     say "$whatever skipped"
+# @todo: Option to show only domain problems vs subdomain problems.
+# @todo: This greps out lines from the file with a #. The idea is to be able to
+#     comment out lines, but any URLs with an anchor tag will be a casualty.
 # @todo: Error number padding to make a pretty line so it doesn't bump to the
 #     right when it goes from 9 to 10.
 
 num_prob=1
+tmp_file=/tmp/$0.`date +%Y%m%d%H%M%s%N`
 
 if [ "$#" -gt 0 ]; then
     if [ "$#" -eq 1 ]; then
@@ -27,35 +23,46 @@ if [ "$#" -gt 0 ]; then
 
     is_itemized=1
     list="$@"
+    skipped=''
 else
+    is_one=`wc -l < $file`
+
     is_itemized=0
     # grep out hash signs so we can comment lines out.
     list=`grep -v '#' $file`
-    is_one=`wc -l < $file`
+    # keep track of what we skipped
+    skipped=`grep '#' $file`
+
 fi
 
 for tld in $list; do
 
     # Check if the certificate is working.
-    if curl -Ls --head --request GET https://$tld | grep "200 OK" > /dev/null; then
+    curl -Ls --head --request GET https://$tld -w %{time_total}s > $tmp_file
+    exe_time=`tail -n 1 $tmp_file`
 
-        # It is - check if it works for subdomains. -L switch tells it to follow
-        # redirects.
+    if grep "200 OK" $tmp_file > /dev/null; then
+
+        # It is working. Check if it works for subdomains. -L switch tells it to
+        # follow redirects.
+        #    Above I changed the curl command to retain the execution time. It's
+        # not being updated with this call, so this will always show the amount
+        # of time it took to get to the no-subdomain site.
         if curl -Ls --head --request GET "https://fake."$tld | grep "200 OK" > /dev/null; then
 
-        # Don't say anything about the domains that worked.
-        echo -n '';
+            # Don't say anything about the domains that worked.
+            echo -n '';
 
         # There was a problem with a subdomain
         else
             # There was only one site being checked - don't bother with the
             # number
             if [ "$is_one" -eq 1 ]; then
-                echo Subdomain problem: https://fake.$tld
+                echo Subdomain problem: https://fake.$tld "   ($exe_time)"
 
             # Checked multiple domain names.
             else
-                echo $num_prob. Subdomain problem: https://fake.$tld
+                echo $num_prob. Subdomain problem: https://fake.$tld "   ($exe_time)"
             fi
             ((num_prob))
         fi
@@ -67,11 +74,11 @@ for tld in $list; do
 
         # If we're just checking one site, then don't list the number.
         if [ "$is_one" -eq 1 ]; then
-            echo Problem: https://$tld
+            echo Problem: https://$tld "   ($exe_time)"
 
         # If we're listing multiple sites, do give a number
         else
-            echo $num_prob. Problem: https://$tld
+            echo $num_prob. Problem: https://$tld "   ($exe_time)"
         fi
 
         ((num_prob++))
@@ -81,3 +88,11 @@ done
 if [ $num_prob -eq 1 ]; then
     echo 'All domains worked'
 fi
+
+if [ "$skipped" == '' ]; then
+    echo -n ''
+else
+    echo Skipped: $skipped
+fi
+
+rm $tmp_file
