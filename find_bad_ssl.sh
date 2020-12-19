c7b72e7cb79eaa04cf966625eed7df4d0c6e70d1
KJ Coop <git@kjcoop.com>
2020-12-19 00:52:51 -0800

    Now includes domain and subdomain load times when the script has to complete both steps

--- find_bad_ssl.sh
+++ find_bad_ssl.sh
@@ -10,9 +10,13 @@ file=~/domain_scripts/domains.txt
 #     comment out lines, but any URLs with an anchor tag will be a casualty.
 # @todo: Error number padding to make a pretty line so it doesn't bump to the
 #     right when it goes from 9 to 10.
+# @todo: Refactor curl logic into a function.
+# @todo: Round load times? printf %.2f $exe_time_tld -- see:
+#     https://askubuntu.com/questions/179898/how-to-round-decimals-using-bc-in-bash#574474
 
 num_prob=1
-tmp_file=/tmp/$0.`date +%Y%m%d%H%M%s%N`
+tmp_tld_file=/tmp/$0_tld_`date +%Y%m%d%H%M%s%N`
+tmp_subd_file=/tmp/$0_subd_`date +%Y%m%d%H%M%s%N`
 
 if [ "$#" -gt 0 ]; then
     if [ "$#" -eq 1 ]; then
@@ -38,17 +42,21 @@ fi
 for tld in $list; do
 
     # Check if the certificate is working.
-    curl -Ls --head --request GET https://$tld -w %{time_total}s > $tmp_file
-    exe_time=`tail -n 1 $tmp_file`
+    curl -Ls --head --request GET https://$tld -w %{time_total} > $tmp_tld_file
+    exe_time_tld=`tail -n 1 $tmp_tld_file`
+    echo "Execution time rounded: "
 
-    if grep "200 OK" $tmp_file > /dev/null; then
+    if grep "200 OK" $tmp_tld_file > /dev/null; then
 
         # It is working. Check if it works for subdomains. -L switch tells it to
         # follow redirects.
         #    Above I changed the curl command to retain the execution time. It's
         # not being updated with this call, so this will always show the amount
         # of time it took to get to the no-subdomain site.
-        if curl -Ls --head --request GET "https://fake."$tld | grep "200 OK" > /dev/null; then
+        curl -Ls --head --request GET "https://fake."$tld -w %{time_total} > $tmp_subd_file
+        exe_time_subd=`tail -n 1 $tmp_subd_file`
+
+        if grep "200 OK" $tmp_subd_file > /dev/null; then
 
             # Don't say anything about the domains that worked.
             echo -n '';
@@ -58,13 +66,13 @@ for tld in $list; do
             # There was only one site being checked - don't bother with the
             # number
             if [ "$is_one" -eq 1 ]; then
-                echo Subdomain problem: https://fake.$tld "   ($exe_time)"
+                echo Subdomain problem: https://fake.$tld "   (domain: $exe_time_tld, subdomain: $exe_time_subd)"
 
             # Checked multiple domain names.
             else
                 echo $num_prob. Subdomain problem: https://fake.$tld "   ($exe_time)"
             fi
-            ((num_prob))
+            ((num_prob++))
         fi
 
     # Certificate is not working. It may also have a subdomain problem, but
@@ -95,4 +103,4 @@ else
     echo Skipped: $skipped
 fi
 
-rm $tmp_file
+rm $tmp_tld_file $tmp_subd_file
